f80be81b85f59a39fbab941364ded6ff62d513c9
Eyioyo Omatsola <eyo.omat@gmail.com>
2024-04-05 01:27:34 -0700

    initial commit

---
+++ src/main/java/com/maids/maids_library/MaidsLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.maids.maids_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidsLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaidsLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maids/maids_library/MaidsLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maids.maids_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidsLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
