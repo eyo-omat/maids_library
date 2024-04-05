ee4975da1dfd8d1c7e31ebd5cbe9bbfd107e68ca
Eyioyo Omatsola <eyo.omat@gmail.com>
2024-04-05 02:33:51 -0700

    Added controller classes for library system

---
+++ src/main/java/com/maids/maids_library/controller/BookController.java
@@ -0,0 +1,9 @@
+package com.maids.maids_library.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/books")
+public class BookController {
+}
